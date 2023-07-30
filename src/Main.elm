a08f2fd11406d34a48cb2284b54af668689a71ae
Ethan Pang <43922849+ethanppl@users.noreply.github.com>
2023-07-31 00:05:12 +0800

    Basic setup, Elm works :)

---
+++ src/Main.elm
@@ -0,0 +1,250 @@
+module Main exposing (main)
+
+import Browser
+import Browser.Events exposing (onKeyDown, onKeyUp)
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css)
+import Json.Decode as Decode
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view >> toUnstyled
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { content : String
+    , target : String
+    , answer : String
+    , englishKey : String
+    , showAnswer : Bool
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model "" "倉" "人戈日口" "OIAR" False
+    , Cmd.none
+    )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [ css [ width (vw 100.0), height (vh 100.0), display inlineFlex, flexDirection column ] ]
+        [ div [ css [ height (vh 50.0), display inlineFlex ] ]
+            [ div [ css [ margin4 auto auto (px 8) auto ] ] [ text model.target ] ]
+        , div [ css [ height (vh 50.0), display inlineFlex ] ]
+            [ div [ css [ margin4 (px 8) auto auto auto ] ] [ outputBox model ] ]
+        ]
+
+
+outputBox : Model -> Html Msg
+outputBox model =
+    if model.showAnswer then
+        div [] [ text <| model.answer ++ " (" ++ model.englishKey ++ ")" ]
+
+    else
+        div [] [ text model.content ]
+
+
+
+-- UPDATE
+
+
+type Msg
+    = PressedLetter Char
+    | Control String
+    | LiftedLetter Char
+    | Ignore
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        PressedLetter ' ' ->
+            checkAnswer model
+
+        PressedLetter '?' ->
+            showAnswer model
+
+        PressedLetter char ->
+            ( { model | content = String.append model.content <| String.fromChar char }, Cmd.none )
+
+        Control "Backspace" ->
+            ( { model | content = String.dropRight 1 model.content }, Cmd.none )
+
+        Control _ ->
+            ( model, Cmd.none )
+
+        LiftedLetter '?' ->
+            hideAnswer model
+
+        LiftedLetter _ ->
+            ( model, Cmd.none )
+
+        Ignore ->
+            ( model, Cmd.none )
+
+
+checkAnswer : Model -> ( Model, Cmd Msg )
+checkAnswer model =
+    if model.content == model.answer then
+        ( { model | content = "" }, Cmd.none )
+
+    else
+        ( model, Cmd.none )
+
+
+showAnswer : Model -> ( Model, Cmd Msg )
+showAnswer model =
+    ( { model | showAnswer = True }, Cmd.none )
+
+
+hideAnswer : Model -> ( Model, Cmd Msg )
+hideAnswer model =
+    ( { model | showAnswer = False }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch
+        [ onKeyDown keyDecoder
+        , onKeyUp keyUpDecoder
+        ]
+
+
+keyDecoder : Decode.Decoder Msg
+keyDecoder =
+    Decode.map toKey (Decode.field "key" Decode.string)
+
+
+keyUpDecoder : Decode.Decoder Msg
+keyUpDecoder =
+    Decode.map toUpKey (Decode.field "key" Decode.string)
+
+
+toKey : String -> Msg
+toKey string =
+    case String.uncons string of
+        Just ( char, "" ) ->
+            PressedLetter <| mapKey <| Char.toLocaleUpper char
+
+        _ ->
+            Control string
+
+
+toUpKey : String -> Msg
+toUpKey string =
+    case String.uncons string of
+        Just ( char, "" ) ->
+            LiftedLetter <| mapKey <| Char.toLocaleUpper char
+
+        _ ->
+            Ignore
+
+
+mapKey : Char -> Char
+mapKey char =
+    case char of
+        'A' ->
+            '日'
+
+        'B' ->
+            '月'
+
+        'C' ->
+            '金'
+
+        'D' ->
+            '木'
+
+        'E' ->
+            '水'
+
+        'F' ->
+            '火'
+
+        'G' ->
+            '土'
+
+        'H' ->
+            '竹'
+
+        'I' ->
+            '戈'
+
+        'J' ->
+            '十'
+
+        'K' ->
+            '大'
+
+        'L' ->
+            '中'
+
+        'M' ->
+            '一'
+
+        'N' ->
+            '弓'
+
+        'O' ->
+            '人'
+
+        'P' ->
+            '心'
+
+        'Q' ->
+            '手'
+
+        'R' ->
+            '口'
+
+        'S' ->
+            '尸'
+
+        'T' ->
+            '廿'
+
+        'U' ->
+            '山'
+
+        'V' ->
+            '女'
+
+        'W' ->
+            '田'
+
+        'X' ->
+            '難'
+
+        'Y' ->
+            '卜'
+
+        'Z' ->
+            '重'
+
+        _ ->
+            char
